d02ba8ff4b837a81102325c7cc03dd80f249ff9f
Lukas Mateffy <github@mateffy.me>
2024-10-20 18:18:33 +0200

    test: remove unused tests

--- tests/CreateColorTest.php
+++ tests/CreateColorTest.php
@@ -52,31 +52,6 @@ test('It can create a color from an rgba array', function (array $rgba, string $
         [[128, 128, 128, 0.5], '#808080', '#8080807f'],
     ]);
 
-test('It can format a color as RGB string', function (array $rgb, string $expected) {
-    $color = Color::rgb(...$rgb);
-    expect($color->toRgbString())->toBe($expected);
-})
-    ->with([
-        [[255, 0, 0], 'rgb(255, 0, 0)'],
-        [[0, 255, 0], 'rgb(0, 255, 0)'],
-        [[0, 0, 255], 'rgb(0, 0, 255)'],
-        [[128, 128, 128], 'rgb(128, 128, 128)'],
-        [[0, 0, 0], 'rgb(0, 0, 0)'],
-        [[255, 255, 255], 'rgb(255, 255, 255)'],
-    ]);
-
-test('It can format a color as RGBA string', function (array $rgba, string $expected) {
-    $color = Color::rgb(...$rgba);
-    expect($color->toRgbaString())->toBe($expected);
-})
-    ->with([
-        [[255, 0, 0, 1], 'rgba(255, 0, 0, 1)'],
-        [[0, 255, 0, 0.5], 'rgba(0, 255, 0, 0.5)'],
-        [[0, 0, 255, 0.25], 'rgba(0, 0, 255, 0.25)'],
-        [[128, 128, 128, 0.75], 'rgba(128, 128, 128, 0.75)'],
-        [[0, 0, 0, 0], 'rgba(0, 0, 0, 0)'],
-        [[255, 255, 255, 1], 'rgba(255, 255, 255, 1)'],
-    ]);
 
 test('It can create a color from an hsl array', function (array $hsl, string $default, string $alpha) {
     $color = Color::hsl(...$hsl);
