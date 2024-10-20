0cdc4847afa6e46935bb360b2b046d779825fea3
Lukas Mateffy <github@mateffy.me>
2024-10-20 18:23:43 +0200

    feat: Add ability to set or add alpha value of color

--- src/Color/Concerns/Modifiers.php
+++ src/Color/Concerns/Modifiers.php
@@ -4,6 +4,15 @@ namespace Mateffy\Color\Concerns;
 
 trait Modifiers
 {
+    /**
+     * Set or add the alpha value of the color.
+     *
+     * @example $color->alpha(0.5)
+     * @example $color->alpha(0.2, add: true)
+     *
+     * @param float $alpha Opacity value between 0.0 and 1.0
+     * @param bool $add Whether to add the value or set it, defaults to false
+     */
 	public function alpha(float $alpha, bool $add = false): static
 	{
         if ($add) {
@@ -15,6 +24,15 @@ trait Modifiers
 		return new static($this->red, $this->green, $this->blue, $alpha);
 	}
 
+    /**
+     * Set or add the opacity of the color. Alias for `alpha`.
+     *
+     * @example $color->opacity(0.5)
+     * @example $color->opacity(0.2, add: true)
+     *
+     * @param float $opacity Opacity value between 0.0 and 1.0
+     * @param bool $add Whether to add the value or set it, defaults to false
+     */
 	public function opacity(float $opacity, bool $add = false): static
 	{
 		return $this->alpha($opacity, add: $add);
--- tests/ModifyColorTest.php
+++ tests/ModifyColorTest.php
@@ -2,69 +2,8 @@
 
 use Mateffy\Color;
 
-test('It can lighten a color', function () {
-    $color = Color::hex('#ff0000');
-    $lightened = $color->lighten(0.2);
-    expect($lightened->toHexString())->toBe('#ff3333');
-});
-
-test('It can darken a color', function () {
-    $color = Color::hex('#ff0000');
-    $darkened = $color->darken(0.2);
-    expect($darkened->toHexString())->toBe('#cc0000');
-});
-
-test('It can saturate a color', function () {
-    $color = Color::hex('#808080');
-    $saturated = $color->saturate(0.5);
-    expect($saturated->toHexString())->toBe('#bf4040');
-});
-
-test('It can desaturate a color', function () {
-    $color = Color::hex('#ff0000');
-    $desaturated = $color->desaturate(0.5);
-    expect($desaturated->toHexString())->toBe('#bf4040');
-});
-
 test('It can adjust the hue of a color', function () {
     $color = Color::hex('#ff0000');
     $hueAdjusted = $color->hue(120);
     expect($hueAdjusted->toHexString())->toBe('#00ff00');
-});
-
-test('It can rotate the hue of a color', function () {
-    $color = Color::hex('#ff0000');
-    $hueRotated = $color->rotate(120);
-    expect($hueRotated->toHexString())->toBe('#00ff00');
-});
-
-test('It can adjust the alpha of a color', function () {
-    $color = Color::hex('#ff0000');
-    $alphaAdjusted = $color->alpha(0.5);
-    expect($alphaAdjusted->toHexString(alpha: true))->toBe('#ff00007f');
-});
-
-test('It can invert a color', function () {
-    $color = Color::hex('#ff0000');
-    $inverted = $color->invert();
-    expect($inverted->toHexString())->toBe('#00ffff');
-});
-
-test('It can mix two colors', function () {
-    $color1 = Color::hex('#ff0000');
-    $color2 = Color::hex('#0000ff');
-    $mixed = $color1->mix($color2, 0.5);
-    expect($mixed->toHexString())->toBe('#800080');
-});
-
-test('It can get the complement of a color', function () {
-    $color = Color::hex('#ff0000');
-    $complement = $color->complement();
-    expect($complement->toHexString())->toBe('#00ffff');
-});
-
-test('It can get the grayscale of a color', function () {
-    $color = Color::hex('#ff0000');
-    $grayscale = $color->grayscale();
-    expect($grayscale->toHexString())->toBe('#4d4d4d');
 });
